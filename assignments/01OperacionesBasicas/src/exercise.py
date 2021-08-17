5d67f53e60b766d448824febe3e79fb62003229b
A01424790FernandoG <A01424790@itesm.mx>
2021-08-17 15:01:01 +0000

    tarea 1

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,7 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-
-
+    n1=int(input("Dame un número: "))
+    n2=int(input("Dame un número: "))
+    suma = n1+n2
+    resta = n1-n2
+    multi = n1*n2
+    print("Suma: ",suma)
+    print("Resta: ",resta)
+    print("Multiplicación: ", multi)
 
 if __name__ == '__main__':
     main()
